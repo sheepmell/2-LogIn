6190fd6b4b782ca33c42bf77c7cd3e89b13ed19e
xin chen <823515512@qq.com>
2020-10-30 16:24:16 +0800

    111

---
+++ 2-LogIn/2-LogIn/frm_LogIn.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _2_LogIn
+{
+    partial class frm_LogIn
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_LogIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.txt_No = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_LogIn
+            // 
+            this.btn_LogIn.Location = new System.Drawing.Point(184, 193);
+            this.btn_LogIn.Name = "btn_LogIn";
+            this.btn_LogIn.Size = new System.Drawing.Size(75, 39);
+            this.btn_LogIn.TabIndex = 0;
+            this.btn_LogIn.Text = "登录";
+            this.btn_LogIn.UseVisualStyleBackColor = true;
+            this.btn_LogIn.Click += new System.EventHandler(this.btn_LogIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "帐号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码：";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(132, 122);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(212, 28);
+            this.txt_Password.TabIndex = 3;
+            // 
+            // txt_No
+            // 
+            this.txt_No.Location = new System.Drawing.Point(132, 64);
+            this.txt_No.Name = "txt_No";
+            this.txt_No.Size = new System.Drawing.Size(212, 28);
+            this.txt_No.TabIndex = 4;
+            // 
+            // frm_LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 274);
+            this.Controls.Add(this.txt_No);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_LogIn);
+            this.Name = "frm_LogIn";
+            this.Text = "登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_LogIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.TextBox txt_No;
+    }
+}
+
---
+++ 2-LogIn/2-LogIn/frm_LogIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartLinli.DatabaseDevelopement;
+
+namespace _2_LogIn
+{
+    public partial class frm_LogIn : Form
+    {
+        public frm_LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_LogIn_Click(object sender, EventArgs e)
+        {
+            string commandText =
+                $@"SELECT 1
+                   FROM tb_Student AS S
+	               WHERE S.No = '{this.txt_No.Text}' 
+                         AND S.Password = '{this.txt_Password.Text}';";
+
+            SqlHelper sqlHelper = new SqlHelper();
+            int result = sqlHelper.QuickReturn<int>(commandText);
+            if (result==1)
+            {
+                MessageBox.Show("登陆成功！");
+            }
+            else
+            {
+                MessageBox.Show("登陆失败！");
+            }
+        }
+    }
+}
